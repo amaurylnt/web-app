759848eaa81c152233830724a75c29667bb6efd9
mehdi <mhdi.seddik@gmail.com>
2023-04-14 00:51:19 +0200

    feat: add websocket

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,33 @@ import iconCrossed from "./coffee-cup-crossed.png";
 
 import { css } from "@emotion/css";
 import "./App.css";
+import useWebSocket from "react-use-websocket";
 
 function App() {
-  const [isUp] = React.useState(false);
+  const [isUp, setIsUp] = React.useState(false);
+  const WS_URL = "ws://127.0.0.1:8999";
+
+  const {
+    sendMessage,
+    sendJsonMessage,
+    lastMessage,
+    lastJsonMessage,
+    readyState,
+    getWebSocket,
+  } = useWebSocket(WS_URL, {
+    shouldReconnect: (closeEvent) => true,
+    onMessage: (message) => {
+      // get json data
+      const data = JSON.parse(message.data);
+      setIsUp(data.coffeeStatus);
+    },
+  });
   return (
     <div className={styles.wrapper(isUp)}>
       <header className="App-header">
         <img src={isUp ? icon : iconCrossed} className="App-logo" alt="logo" />
         <p>
-          {isUp ? "La machine est opérationnelle" : "la machine est en panne"}
+          {isUp ? "La machine est opérationnelle" : "La machine est en panne"}
         </p>
       </header>
     </div>
